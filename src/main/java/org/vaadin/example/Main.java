a3f73135d89da53a56ae7d1370f1d6c1640bd3f9
bp20021 <bp20021@shibaura-it.ac.jp>
2022-01-11 07:47:23 +0000

    updated

--- src/main/java/org/vaadin/example/Main.java
+++ src/main/java/org/vaadin/example/Main.java
@@ -24,6 +24,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 @CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
 public class Main extends VerticalLayout {
     UI ui;
+    
     static final AtomicInteger count23 = new AtomicInteger(0);
 
 
